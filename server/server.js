e247b22fb7b8157c1732bf093ff86fc11ef3670b
Roger Ng <roger.ng@3plearning.com>
2019-03-24 00:15:48 +1100

    www modified

--- server/server.js
+++ server/server.js
@@ -1,13 +1,22 @@
 import express from 'express';
 import path from 'path';
-
 import routes from './routes/index';
 
 const app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use('/', routes);
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
 
 module.exports = app;
