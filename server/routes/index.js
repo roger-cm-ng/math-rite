d4a84cb9b7dbd4bc851e679d7214f89d7ee6d396
Roger Ng <roger.ng@3plearning.com>
2019-03-23 23:21:59 +1100

    heroku settings

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,7 +1,9 @@
 import express from 'express';
+
 const Index = express.Router();
 
 Index.get('/', (req, res) => {
   res.render('index');
 });
+
 export default Index;
--- server/server.js
+++ server/server.js
@@ -1,28 +1,13 @@
 import express from 'express';
-import http from 'http';
-import socketIo from 'socket.io';
 import path from 'path';
 
 import routes from './routes/index';
 
 const app = express();
-const server = http.Server(app);
-const io = socketIo(server);
-const port = 3000;
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-server.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
-
 app.use('/', routes);
 
-io.on('connection', (socket) => {
-  console.log('user connected');
-  socket.emit('message', { roger: 'hey how are you?'});
-  socket.on('another event', (data) => {
-    console.log(data);
-  });
-});
+module.exports = app;
