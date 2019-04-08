94705d7f2a46ee58ab9e25e0424985dfa1c84bc4
3PAUS44\Roger Ng <roger.ng@3plearning.com>
2019-04-08 17:56:22 +1000

    pacifico

--- components/header/header.js
+++ components/header/header.js
@@ -15,7 +15,10 @@ class Header extends Component {
     const { history } = this.props;
     return (
       <div className={css.header}>
-        <div className={css.logo}>MATH-RiTE</div>
+        <div className={css.logo}>
+          <span className={css.math}>Math</span>
+          <span className={css.rite}>Rite</span>
+        </div>
         <div className={css.separator} />
         <ul className={css.shell}>
           <li
